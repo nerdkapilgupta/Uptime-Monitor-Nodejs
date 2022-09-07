3ed43612dd3d970047981ee7337a0aa6e95ee005
nerdkapilgupta <84197574+nerdkapilgupta@users.noreply.github.com>
2022-09-07 09:51:55 +0530

    adding main files

---
+++ mailer.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+const Mailgun = require('mailgun-js');
+const config = require('./config');
+const subject = 'Website Down';
+const emailFooter = '<br><br><br><a href="http://node-ping.herokuapp.com">Ping Monitor</a>';
+
+
+module.exports.sendEmail = function(opts, fn) {
+
+  let mailgun = new Mailgun({
+    apiKey: config.mailgun.apiKey,
+    domain: config.mailgun.domain
+  });
+
+  let data = {
+    from: config.mailgun.email,
+    to: config.deliveryEmail,
+    subject: opts.subject || subject,
+    html: opts.body + emailFooter
+  };
+
+  mailgun.messages().send(data, fn);
+};
